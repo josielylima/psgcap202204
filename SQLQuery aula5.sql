761d1cb9349b74532653b08a8d88f1cb9684abf6
Josiely Duarte <100868307+josielylima@users.noreply.github.com>
2022-04-08 14:23:37 -0400

    inclusão da aula5

---
+++ SQLQuery aula5.sql
@@ -0,0 +1,17 @@
+SELECT * FROM Produto
+GO
+
+
+INSERT INTO Produto(ID_Produto, Descricao_Produto, Preco_Produto)
+VALUES 
+         (3,'Fanta Laranja lata 350ml',4.50),
+         (4,'Fanta Uva lata 350ml',3.50),
+		 (5,'Sprint lata 150ml',1.50),
+		 (6,'Pepsi lata 350ml',3.50),
+		 (7,'Pepsi Twist lata 150ml',1.50),
+		 (8,'Tubaina Funada lata 250ml',2.50),
+		 (9,'Guarana Antartica lata 350ml',4.50),
+		 (10,'Dolly lata 150ml',1.50)
+Go
+
+
